d06a4b89d553a2722a8ca016286222f04c5fb23a
platonovsimeon <41298320+platonovsimeon@users.noreply.github.com>
2019-01-29 16:50:05 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 from GAN import GAN
 #Creates a new GAN. Parameter howOftenToSave specifies how many epochs there are between savings.
 gan =  GAN(height=128,width=128,channel=3,batchSize=20,epochs=100,versionName="New art",dataFolder="data",howOftenToSave=50)
-#Trains the model. You need to run this only once. Then you can use the model that was saved during the training.
+#Trains the model. This step is not necessary if you have a trained model already in the folder "model/versionName".
 gan.train()
 #Generates a new image. Parameter epochToRestore specifies which save to restore. 
 gan.generateNew(epochToRestore=100)
